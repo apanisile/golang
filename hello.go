826f8cefc66bf91a8642087760bea5baa41850b4
apanisile <apanisileifeoluwa@gmail.com>
2021-03-02 07:03:04 +0100

    learning variables

--- hello.go
+++ hello.go
@@ -2,8 +2,31 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
+	//var batman string = "I am batman"
+	//fmt.Println(batman)
 
-  fmt.Println("Hello Ifoeluwa")
+	//var superman string
+	//superman = "I am superman"
+	//fmt.Println(superman)
+
+	//thor := "I am thor"
+	//fmt.Println(thor)
+
+	//var defaultValue string
+	//fmt.Println(defaultValue)
+
+	//var (
+	//	spiderman = "I am spiderman"
+	//	age       = 18
+	//	powers    = "web slings, spidy sense"
+	//)
+	//fmt.Println(spiderman, age, powers)
+
+	fmt.Print("Enter your name: ")
+	var input string
+	fmt.Scanf("%s", &input)
+
+	fmt.Println(output)
 
 }
