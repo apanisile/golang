624b4a27573dc08cb45801467c831dc97cc2c2cc
apanisile <apanisileifeoluwa@gmail.com>
2021-03-09 11:03:55 +0100

    learning golang

--- hello.go
+++ hello.go
@@ -23,10 +23,44 @@ func main() {
 	//)
 	//fmt.Println(spiderman, age, powers)
 
-	fmt.Print("Enter your name: ")
-	var input string
-	fmt.Scanf("%s", &input)
+	//fmt.Print("Enter your name: ")
+	//var input string
+	//fmt.Scanf("%s", &input)
 
-	fmt.Println(output)
+	//fmt.Println(input)
+
+	//for i := 1; i <= 10; i++ {
+	//fmt.Println(i)
+	//	if i%2 == 0 {
+	//even number
+	//		fmt.Println(i, "is an even number")
+	//	} else {
+	//odd
+	//		fmt.Println(i, "is an odd number")
+	//	}
+	//}
+
+	//var x [5]float64
+	//x[0] = 98
+	//x[1] = 93
+	//x[2] = 77
+	//x[3] = 82
+	//x[4] = 83
+
+	//var total float64 = 0
+	//for i := 0; i < len(x); i++ {
+	//		total += x[i]
+	//		fmt.Println(i)
+	//	}
+	//	fmt.Println(total / float64(len(x)))
+
+	//slice1 := []int{1, 2, 3}
+	//slice2 := make([]int, 2)
+	//copy(slice2, slice1)
+	//fmt.Println(slice1, slice2)
+
+	x := make(map[string]int)
+	x["key"] = 10
+	fmt.Println("%v %T", x, x)
 
 }
